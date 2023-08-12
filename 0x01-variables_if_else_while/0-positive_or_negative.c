a6d3c73331b7d972fc63bb51b9a47402bc438689
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-12 22:01:07 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d is", n);
-	if(n > 0)
+	if (n > 0)
 		 printf(" positive\n");
-	else if(n == 0)
-		 printf(" zero\n");
+	else if (n == 0)
+	{printf(" zero\n");}
 	else 
-		printf(" negative\n");
-
+	{printf(" negative\n");}
 	return (0);
 }
 
