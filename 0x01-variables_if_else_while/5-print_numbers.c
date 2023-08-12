01efb0f21464071dc89a4f2c49ea4d6ae0feff84
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-12 23:00:11 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,12 +3,12 @@
  * main - main block
  * Description: Print all numbers of base 10, starting from 0.
  * Return: 0
-*/  
+ */  
 int main(void)
 {
 char digit;
 
-for(digit = '0'; digit <= '9'; digit++)
+for (digit = '0'; digit <= '9'; digit++)
 putchar (digit);
 
 
