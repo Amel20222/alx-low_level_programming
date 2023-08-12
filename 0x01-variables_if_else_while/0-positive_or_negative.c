22ab577c77a20922edfbc3088efea01fef4e0d56
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-12 22:03:25 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,9 @@ int main(void)
 	if (n > 0)
 		 printf(" positive\n");
 	else if (n == 0)
-	printf(" zero\n");
+		printf(" zero\n");
 	else 
-	printf(" negative\n");
+		printf(" negative\n");
 	return (0);
 }
 
