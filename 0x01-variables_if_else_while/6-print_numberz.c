aaf778e951bf11633ed47e53bf2ea0c9df43beae
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-12 23:04:41 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-	int digit ;
-	
-	for (digit = 0; digit <=9; digit++)
-        	putchar(digit + '0');
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar(digit + '0');
 
 
 	putchar ('\n');
