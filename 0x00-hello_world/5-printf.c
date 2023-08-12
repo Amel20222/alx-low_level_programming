e8f2ca0c3a6d31b9d0997a81a4bf5d7edb200734
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-12 21:07:14 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,9 @@
  * Description: prints "with proper grammar, but the outcome is a piece
  * of art,"
  * Return: Always 0 (Success)
- */
- 
+ */ 
 int main(void)
 {
-	 printf("With proper grammar, but the outcome is a piece of art,\n");
-	 return (0);
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
