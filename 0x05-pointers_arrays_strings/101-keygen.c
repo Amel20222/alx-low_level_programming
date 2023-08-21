a4631d250bf8dcfb8a32f3c23079faa69f57655c
Amel20222 <119702646+Amel20222@users.noreply.github.com>
2023-08-21 18:01:18 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - generates keygen.
- * Return: 0 Always.
- */
-int main(void)
-{
-	int r = 0, c = 0;
-	time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+#define PASSWORD_LENGTH 20
+
+int main(void) {
+    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(NULL)); // Seed the random number generator with the current time
+
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the string
+
+    printf("%s\n", password);
+
+    return 0;
 }
